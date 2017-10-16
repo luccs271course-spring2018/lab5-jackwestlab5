ad7bb647684df8e575da031fdc4389a0cda22864
jwest1 <jwest1@luc.edu>
2017-10-16 08:25:44 -0500

    Fixed reverse lines to work

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -8,26 +8,24 @@ public class ReverseLines {
 
 	public static void main(String[] args) {
 		// TODO read successive input lines until EOF, then print out in reverse order
-		LinkedStack<Character> stack = new LinkedStack<Character>();
+		LinkedStack<String> stack = new LinkedStack<String>();
 
 
     final Scanner input = new Scanner(System.in);
 		String line = null;
-		while ((line = input.nextLine()) != null) {
-
-			System.out.println(line);
-			for(int i = 0; i <line.length(); i++) {
-				stack.push(line.charAt(i));
-			}
-
-
-			String tempWord= "";
-			for(int i = 0; i <line.length(); i++) {
-				tempWord += stack.pop();}
-			System.out.println(tempWord);
-
-
+		while (input.hasNextLine())
+		{
+			line = input.nextLine();
+			stack.push(line);
 		}
+
+		String tempWord= "";
+		while( !stack.isEmpty())
+		{
+			tempWord += "\n" + stack.pop();
+		}
+
+		System.out.println(tempWord);
 
 
 
